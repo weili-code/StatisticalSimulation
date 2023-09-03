0587a44269462839cc8665240053b539edb31511
mac-weili <125315460+weili-code@users.noreply.github.com>
2023-09-03 11:13:02 -0400

    major update on PCA Lec4; delete Lec 5.

--- code/principal_compoenents.R
+++ code/principal_compoenents.R
@@ -1,43 +1,78 @@
 ### This script contains some examples for Principal Components###
 
+# ref. http://www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/112-pca-principal-component-analysis-essentials/
 
-set.seed(123)
-n <- 10
-mean <- c(0, 0)
-sigma <- matrix(c(3, 1.5, 1.5, 2), 2)  # Covariance matrix
-X <- MASS::mvrnorm(n, mean, sigma)
+#####################################
+####### PCA     ############# #######
+#####################################
 
-plot(X[,1], X[,2], xlab="X1", ylab="X2", main="Simulated Data with Principal Components")
+# Load required library
+library(MASS)
 
-cov_X <- cov(X)
-eigenvalues_eigenvectors <- eigen(cov_X)
-eigenvectors <- eigenvalues_eigenvectors$vectors
+# 1. Generate 2D data using bivariate normal
+set.seed(123)  # For reproducibility
+mu <- c(5, 5)  # mean for x and y
+sigma <- matrix(c(4, 2, 2, 3), 2)  # covariance matrix
 
-arrows(0, 0, eigenvectors[1,1]*3, eigenvectors[2,1]*3, col="red", lwd=2)
-arrows(0, 0, eigenvectors[1,2]*3, eigenvectors[2,2]*3, col="blue", lwd=2)
+data <- mvrnorm(50, mu, sigma)
+x <- data[,1]
+y <- data[,2]
 
-pca <- prcomp(X, center = TRUE, scale. = FALSE)
-Z <- pca$x
+# Get the range for both x and y axes
+plot_range <- range(c(x, y))
 
-projected1 <- X %*% matrix(eigenvectors[,1], ncol = 1)
-projected2 <- X %*% matrix(eigenvectors[,2], ncol = 1)
+# 2. Plotting the Scatter Plot
+plot(x, y, xlab="X", ylab="Y", pch=19, main="Scatter Plot with PCA Directions", xlim=plot_range, ylim=plot_range)
 
-segments(X[,1], X[,2], projected1 * eigenvectors[1,1], projected1 * eigenvectors[2,1], col = "red", lty=2)
-segments(X[,1], X[,2], projected2 * eigenvectors[1,2], projected2 * eigenvectors[2,2], col = "blue", lty=2)
+# 3. Calculating Eigenvectors using PCA
+pca_result <- prcomp(data, center=TRUE, scale.=TRUE)
+eigenvectors <- pca_result$rotation[, 1:2]
+
+# 4. Plotting the Eigenvectors with transparent colors
+# Arrows for the positive direction of the PCs
+arrows(mean(x), mean(y), mean(x) + eigenvectors[1, 1]*2, mean(y) + eigenvectors[2, 1]*2, col=rgb(1,0,0,alpha=0.5), lwd=2)
+arrows(mean(x), mean(y), mean(x) + eigenvectors[1, 2]*2, mean(y) + eigenvectors[2, 2]*2, col=rgb(0,0,1,alpha=0.5), lwd=2)
+# Arrows for the negative direction of the PCs
+arrows(mean(x), mean(y), mean(x) - eigenvectors[1, 1]*2, mean(y) - eigenvectors[2, 1]*2, col=rgb(1,0,0,alpha=0.5), lwd=2)
+arrows(mean(x), mean(y), mean(x) - eigenvectors[1, 2]*2, mean(y) - eigenvectors[2, 2]*2, col=rgb(0,0,1,alpha=0.5), lwd=2)
+
+legend("topleft", legend=c("1st PC", "2nd PC"), col=c(rgb(1,0,0,alpha=0.5), rgb(0,0,1,alpha=0.5)), lwd=2)
+
+# 
+selected_point <- data[25,]
+
+# Plot the selected point in green
+points(selected_point[1], selected_point[2], col="green", pch=19, cex=1.5)
+
+# Calculate the projections of the selected point onto the PCA directions
+proj_PC1 <- sum((selected_point - c(mean(x), mean(y))) * eigenvectors[,1]) * eigenvectors[,1] + c(mean(x), mean(y))
+proj_PC2 <- sum((selected_point - c(mean(x), mean(y))) * eigenvectors[,2]) * eigenvectors[,2] + c(mean(x), mean(y))
+
+# Plot the projection on PC1 using a red dashed line and mark the end point with a red circle
+segments(selected_point[1], selected_point[2], proj_PC1[1], proj_PC1[2], col="red", lty=2)
+points(proj_PC1[1], proj_PC1[2], col="red", pch=19, cex=1.2)
+
+# Plot the projection on PC2 using a blue dashed line and mark the end point with a blue circle
+segments(selected_point[1], selected_point[2], proj_PC2[1], proj_PC2[2], col="blue", lty=2)
+points(proj_PC2[1], proj_PC2[2], col="blue", pch=19, cex=1.2)
 
 
+#####################################
+####### Dataset ############# #######
+#####################################
 
-##### Dataset ############# 
+# The functions prcomp() uses the singular value decomposition (SVD).
 
 # Load necessary libraries
-install.packages("ggplot2")
 library(ggplot2)
 
 # Load the mtcars dataset
 data(mtcars)
+mtcars <- mtcars[, -1] # remove column "mpg"
 summary(mtcars)
 
 # 1. Data Preparation
+# In principal component analysis, variables are often scaled (i.e. standardized). 
 # Scale the data to have mean 0 and unit variance
 scaled_mtcars <- scale(mtcars)
 
@@ -70,7 +105,11 @@ ggplot(df_pca, aes(x=PC1, y=PC2, label=Car)) +
   theme(legend.position="none")  
 # Individuals with a similar profile are grouped together.
 
+#####################################
 # spree plot and plot of variances explained
+#####################################
+
+# Eigenvalues can be used to determine the number of principal components to retain after PCA 
 
 # (1). Scree Plot
 eigenvalues <- pca_result$sdev^2
@@ -82,6 +121,7 @@ plot(eigenvalues, type="b", main="Scree Plot",
      pch=19, col="blue")
 
 # (2). Variance Explained Plot
+
 prop_varex <- eigenvalues / sum(eigenvalues)
 cum_varex <- cumsum(prop_varex)
 
@@ -92,17 +132,18 @@ plot(cum_varex, type="b", main="Variance Explained Plot",
      pch=19, col="red", ylim=c(0,1))
 abline(h=0.95, col="blue", lty=2) # line at 95% variance explained
 
-# In the scree plot, you should look for an "elbow" where the 
-# eigenvalues start to level off. The principal components to the 
-# left of this elbow retain most of the useful information, and those to the right 
-# can often be omitted without losing much information.
+# In the scree plot, search for a noticeable "bend" where the eigenvalues 
+# begin to stabilize. The principal components situated before this bend capture 
+# the bulk of the essential data, while those after it can frequently be disregarded 
+# without sacrificing significant information.
 # 
-# In the variance explained plot, the aim is often to determine how many 
-# principal components are needed to explain a certain proportion of the 
-# variance (e.g., 95%). The horizontal line at 0.95 will help you visually identify this.
+# For the variance explained graph, the goal is typically to identify the number 
+# of principal components required to account for a specific percentage of the variance, 
+# like 95%. A horizontal line drawn at 0.95 will assist in visually pinpointing this.
 
-####
-####
+#####################################
+####. variables correlation plot ####
+#####################################. 
 
 # 1. Extract Loadings
 # pca_result$rotation: the matrix V, num_variables x num_pcs
@@ -118,10 +159,49 @@ plot(loadings, type="n", xlim=c(-1,1), ylim=c(-1,1),
 abline(h=0, v=0, col="gray", lty=2)
 
 # Add vectors
-arrows(0, 0, loadings[,1], loadings[,2], length=0.1, angle=10, col="blue")
+arrows(0, 0, loadings[,1], loadings[,2], angle=10, col="blue")
 
 # Add text labels
 text(loadings, labels=rownames(loadings), cex=0.7, pos=3)
 
 # Add a circle with radius 1
 symbols(x=0, y=0, circles=1, add=TRUE, inches=FALSE, lty=2)
+
+# Variables that are close to each other are positively correlated.
+# Variables that are orthogonal (perpendicular) to each other are not correlated.
+# Variables that are on opposite ends of a plot (e.g., opposite sides of the origin along the same line) are negatively correlated.
+# The closer a variable's loading is to the edge of the plot (a circle of radius 1), the more influence it has on that particular principal component.
+# Variables that are closed to the center of the plot are less important for the first few components.
+
+#####################################
+###### contribution of variables ####
+#####################################
+
+## (1) contribution of the variable ##
+
+# Compute squared loadings
+squared_loadings <- pca_result$rotation^2 # num_case * num_pcs
+
+# Compute the contribution of each variable to PC1 and PC2
+contribution_PC1 <- squared_loadings[,1] / sum(squared_loadings[,1])
+contribution_PC2 <- squared_loadings[,2] / sum(squared_loadings[,2])
+
+# Print the contributions
+print(data.frame( Contribution_to_PC1 = contribution_PC1, 
+                 Contribution_to_PC2 = contribution_PC2))
+
+## (2) contribution of the variable in variance ##
+
+# Get eigenvalues (variances explained by each PC)
+eigenvalues <- pca_result$sdev^2
+
+# Compute the weighted total contribution of each variable across all PCs
+weighted_total_contribution <- colSums(squared_loadings %*% diag(eigenvalues))
+
+# Compute proportional total contribution
+proportional_weighted_total_contribution <- weighted_total_contribution / sum(eigenvalues)
+
+# Print the weighted total contributions
+print(data.frame(Variable = rownames(squared_loadings), 
+                 Weighted_Total_Contribution = weighted_total_contribution, 
+                 Proportional_Weighted_Total_Contribution = proportional_weighted_total_contribution))
